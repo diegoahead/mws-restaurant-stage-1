f9d03fdc6ce0a64941bfafbcce75caaacac973ea
Diego R. Palacios Lepore <diegopalacios3@gmail.comgit>
2018-01-20 14:49:23 -0400

    Added local an remot urls.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,7 +154,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/mws-restaurant-stage-1/img/${restaurant.photograph}`);
   }
 
   /**
