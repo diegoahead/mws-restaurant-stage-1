0ff3388e6646f55e5045987a192f080b261085fd
Diego R. Palacios Lepore <diegopalacios3@gmail.comgit>
2018-01-20 15:06:47 -0400

    Added local an remot urls.

--- sw.js
+++ sw.js
@@ -41,7 +41,6 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
